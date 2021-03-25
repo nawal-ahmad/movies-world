2836f4a63b2fb44e23f4f9183846f1592b4d6b36
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-03-25 15:42:17 +0200

    Adding JavaScript file

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,16 +1,15 @@
 
-
 var userName = prompt ("What is your name?");
 var time = prompt ("what time is it in 24 hour time?");
 
 if (time < 12){
-  alert ('Good morning ' + userName);
+  alert('Good morning ' + userName);
 }
 else if (12<time && time<17){
   alert('Good afternoon ' + userName);
 }
 else if (17<time && time<24){
-  alert ('Good evening ' + userName);
+  alert('Good evening ' + userName);
 }
 else {
   alert('Welcome to my website ' + userName);
@@ -18,4 +17,4 @@ else {
 
 confirm ('Are you over 18 years old?') 
 
-document.write ('<img src="https://cdn.dribbble.com/users/23012/screenshots/2994574/wlecome.gif">')
+document.write ('<img src="https://cdn.dribbble.com/users/23012/screenshots/2994574/wlecome.gif">');
