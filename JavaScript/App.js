d86246307388e003a357d8f45304acf49ec474cd
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-03-28 14:52:48 +0300

    using loops

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,24 +1,37 @@
+
 var userName = prompt ("What is your name?");
 var favMovie= prompt('Whats your favourite movies type!','comedy , action , horror');
 confirm ('Are you over 18 years old?') 
 
-if (favMovie ==='comedy'){
-  document.write('Welcome to the movies world ' + userName + ', Here is a suggestion for one of the best comedy movies     ');
-
-  document.write ('<img src="https://m.media-amazon.com/images/M/MV5BODViZDg3ZjYtMzhiYS00YTVkLTk4MzktYWUxMTlkYjc1NjdlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY209_CR0,0,140,209_AL_.jpg">');
-}
-else if (favMovie ==='action'){
-  document.write('Welcome to the movies world ' + userName + ', Here is a suggestion for one of the best action movies     ');
-
-  document.write('<img src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY209_CR0,0,140,209_AL_.jpg">');
-}
-else if (favMovie ==='horror'){
-  document.write('Welcome to the movies world ' + userName + ', Here is a suggestion for one of the best horror movies      ');
-
-  document.write('<img src="https://m.media-amazon.com/images/M/MV5BMTAxMDE4Mzc3ODNeQTJeQWpwZ15BbWU4MDY2Mjg4MDcx._V1_UY209_CR1,0,140,209_AL_.jpg">');
-}
-else {
-  alert('Welcome to the movies world ' + userName);
+var answer = 13;
+var x = prompt ("Just to improve you are human, Please answer the following equation: x=5*2+3, x equal:");
+ 
+while (x != answer) {
+  x = prompt ("Please try again x=5*2+3, x equal:");
 }
 
+console.log (x, answer);
 
+var moviesNum = prompt('How many movies pictures would you like to see?');
+
+
+for(var i = 1 ; i <=moviesNum; i++ ){
+  if (favMovie ==='comedy'){
+    document.write ('<img src="https://m.media-amazon.com/images/M/MV5BODViZDg3ZjYtMzhiYS00YTVkLTk4MzktYWUxMTlkYjc1NjdlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY209_CR0,0,140,209_AL_.jpg">');
+
+  }
+  else if (favMovie ==='action'){
+
+    document.write('<img src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY209_CR0,0,140,209_AL_.jpg">');
+  }
+  else if (favMovie ==='horror'){
+
+    document.write('<img src="https://m.media-amazon.com/images/M/MV5BMTAxMDE4Mzc3ODNeQTJeQWpwZ15BbWU4MDY2Mjg4MDcx._V1_UY209_CR1,0,140,209_AL_.jpg">');
+  }
+  else {
+    alert('Welcome to the movies world ' + userName);
+  }
+
+}
+
+console.log(image);
