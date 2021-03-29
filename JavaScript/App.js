259f7051de0596182cee08914a522085ea93cc34
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-03-29 16:58:29 +0300

    used function

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,15 +1,19 @@
-
 var favMovie= prompt('Whats your favourite movies type!','comedy , action , horror');
 confirm ('Are you over 18 years old?') 
 
-var answer = 13;
+var answer=13;
 var x = prompt ("Just to improve you are human, Please answer the following equation: x=5*2+3, x equal:");
- 
-while (x != answer) {
-  x = prompt ("Please try again x=5*2+3, x equal:");
-}
 
+function equAns()
+{
+  while (x != answer) {
+    x = prompt ("Please try again x=5*2+3, x equal:");
+  }
+  return 5*2+3; 
+}
+equAns()
 console.log (x, answer);
+
 
 var moviesNum = prompt('How many movies pictures would you like to see?');
 
@@ -27,9 +31,7 @@ for(var i = 1 ; i <=moviesNum; i++ ){
     document.write('<img src="https://m.media-amazon.com/images/M/MV5BMTAxMDE4Mzc3ODNeQTJeQWpwZ15BbWU4MDY2Mjg4MDcx._V1_UY209_CR1,0,140,209_AL_.jpg">');
   }
   else {
-    alert('Welcome to the movies world ' + userName);
+    alert('Welcome to the movies world ');
   }
 
 }
-
-console.log(image);
